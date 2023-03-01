cfa314eac659bb9df5c706abcde009f6d1bf9a20
dzhuraa <d.zhurakivsky@gmail.com>
2023-03-01 15:31:53 +0200

    commit

---
+++ hw.py
@@ -0,0 +1,22 @@
+
+# 1)написати прогу яка вибирає зі введеної строки цифри і виводить їх через кому.
+
+
+
+# input_string = input("Enter letters and numbers: ")
+#
+# num = []
+#
+# for i in input_string:
+#     if i.isdigit():
+#         num.append(i)
+# print(','.join(num))
+
+
+
+####
+
+
+
+
+
